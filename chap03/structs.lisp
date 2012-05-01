5e797994fc48ae4132a8587d77f6dc6f93314882
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-01 04:07:08 -0700

    lisp structs 101

---
+++ chap03/structs.lisp
@@ -0,0 +1,26 @@
+
+; Struct definition
+(defstruct name
+	first-name
+	(middle-name nil)
+	last-name)
+
+; Create a value, and set it to a var
+;    the constructor function make-(struct-name) is automatically created.
+;    the literal representation of this object is: 
+;        #S(name :first-name fabrice :middle-name nil :last-name leal)
+;    and the ":middle-name nil" part is optional
+(setf b (make-name :first-name 'Fabrice :last-name 'Leal))
+
+; Acessors (struct-name)-(struct-field) are automatically created
+(print (name-first-name b))
+(print (name-middle-name b))
+(print (name-last-name b))
+
+; A type-predicate (struct-name)-p is automatically created
+(print (name-p b))
+
+; You can set the value of a field like this
+(setf (name-middle-name b) 'Ferreira)
+
+(print b)
