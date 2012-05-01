df3f5228872d33f16eef0528ca3e8da6f1de94aa
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-01 07:38:50 -0700

    on debugging and errors (incomplete ...) ...

---
+++ chap03/dbg_errors.lisp
@@ -0,0 +1,15 @@
+
+; apropos: prints information about all objects whose names match the argument
+; (apropos 'string)
+
+; describe: prints information about an object
+; (describe 'make-string)
+; (describe 17)
+
+; documentation: prints the symbol's doc string
+; (documentation 'first 'function)
+; (documentation 'pi 'variable)
+
+; inspect: ...
+
+
