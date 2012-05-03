cee26f485d39bbb48b1b1cc11ddddc3ffd3874fa
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-03 06:01:44 -0700

    finished on errors ...

--- chap03/dbg_errors.lisp
+++ chap03/dbg_errors.lisp
@@ -12,4 +12,38 @@
 
 ; inspect: ...
 
+; Drastic average (error ...)
+(defun drastic-average(numbers)
+	(if (null numbers)
+		(error "Average of the empty list is undefined.")
+		(/ (reduce #'+ numbers)
+			(length numbers) ) ) )
+
+; Less-drastic average (cerror ...)
+(defun cool-average (numbers)
+	(if (null numbers)
+		(progn
+			(cerror "Use 0 as the average"
+				"Average of the empty list is undefined") 
+			0)
+		(/ (reduce #'+ numbers) (length numbers)) ) )
+
+; Usage of (check-type ...)
+(defun sqr-check-type (x)
+	"Multiply x by itself"
+	(check-type x number)
+	(* x x))
+
+; Usage of (assert ...)
+(defun sqr-assert (x)
+	"Multiply x by itself."
+	(assert (numberp x))
+	(* x x))
+
+; Usage of (time ...): interpreted vs compiled
+
+(defun f(n) (dotimes (i n) nil))
+(time (f 1000))
+(compile 'f)
+(time (f 1000))
 
