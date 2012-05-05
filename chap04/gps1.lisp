03f8366b0f28634ca36a342a56249a3830bb1cce
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-05 05:21:14 -0700

    looks ok.

--- chap04/gps1.lisp
+++ chap04/gps1.lisp
@@ -4,13 +4,19 @@
 
 ;;;; File gps1.lisp: First version of GPS (General Problem Solver)
 
+(load "../chap03/onparameters.lisp") ; (find-all ...)
+
 (defvar *state* nil "The current state: a list of conditions.")
 
 (defvar *ops* nil "A list of available operators.")
 
 (defstruct op "An operation"
-  (action nil) (preconds nil) (add-list nil) (del-list nil))
+  (action nil) (preconds nil) (add-list nil) (del-list nil) )
 
+; (GPS 1 2 3)
+; 1 - current state of the world
+; 2 - goal state
+; 3 - list of allowable operators
 (defun GPS (*state* goals *ops*)
   "General Problem Solver: achieve all goals using *ops*."
   (if (every #'achieve goals) 'solved))
@@ -18,14 +24,17 @@
 (defun achieve (goal)
   "A goal is achieved if it already holds,
   or if there is an appropriate op for it that is applicable."
-  (or (member goal *state*)
+  (or (member goal *state*) ; The goal is already in the current state
       (some #'apply-op 
-            (find-all goal *ops* :test #'appropriate-p))))
+            (find-all goal *ops* :test #'appropriate-p) ) ) )
 
+; An operation is apropriate if the goal is in the add-list of op
 (defun appropriate-p (goal op)
   "An op is appropriate to a goal if it is in its add list."
   (member goal (op-add-list op)))
 
+; If all the preconditions for an operator can be achieved,
+; update state after applying the operator to the current state
 (defun apply-op (op)
   "Print a message and update *state* if op is applicable."
   (when (every #'achieve (op-preconds op))
