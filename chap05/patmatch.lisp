3ca1f4bd65689afeb748bd40f3ce4835fc19a410
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-12 10:08:55 -0700

    working pat-match with variable matching

--- chap05/patmatch.lisp
+++ chap05/patmatch.lisp
@@ -6,15 +6,48 @@
 		(and 	(simple-equal (first x) (first y))
 			(simple-equal (rest x) (rest y)) ) ) )
 
-(defun pat-match (pattern input)
+;; Some constants
+
+(defconstant fail nil "Indicates pat-match failure")
+(defconstant no-bindings '((t . t)) 
+	"Indicates pat-match success, with no vars")
+
+;; Abstractions on the bindings list
+
+(defun get-binding (var bindings)
+	"Fing a (var . value) pair in a binding list."
+	(assoc var bindings) )
+
+(defun binding-val (binding)
+	"Get the value part of a single binding."
+	(cdr binding) )
+
+(defun lookup (var bindings)
+	"Get the value part (for var) from a binding list."
+	(binding-val (get-binding var bindings)) )
+
+(defun extend-bindings (var val bindings)
+	"Add a (var . val) pair to a binding list."
+	(cons (cons var val) 
+		(if (eq bindings no-bindings) nil bindings) ) )
+
+;; The pat-match function
+
+(defun pat-match (pattern input &optional (bindings no-bindings))
 	"Does pattern match input? Any variable can match anything."
-	(if (variable-p pattern)
-		(list (cons pattern input))
-		(if (or (atom pattern) (atom input)) 
-			(eql pattern input)
-			(append
-				(pat-match (first pattern) (first input))
-				(pat-match (rest pattern) (rest input)) ) ) ) )
+	(cond ((eq bindings fail) fail)
+		((variable-p pattern) (match-variable pattern input bindings))
+		((eql pattern input) bindings)
+		((and (consp pattern) (consp input)) 
+			(pat-match (rest pattern) (rest input)
+				(pat-match (first pattern) (first input) bindings) ) ) ) )
+
+(defun match-variable (var input bindings)
+	"Does var match input? Uses or updates and returns bindings"
+	(let ((binding (get-binding var bindings))) 
+		(cond ((not binding) (extend-bindings var input bindings))
+			((equal input (binding-val binding)) bindings)
+			(t fail) ) ) )
 
 (defun variable-p (x)
 	"Is x a variable (a symbol beginning with ?)"
